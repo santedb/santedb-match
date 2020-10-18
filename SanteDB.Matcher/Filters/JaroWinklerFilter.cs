94f68630fcd7cd0e03723793693b92a8ce1e745b
Justin Fyfe <justin@fyfesoftware.ca>
2020-10-18 17:09:07 -0400

    Added jaro-winkler functions

---
+++ SanteDB.Matcher/Filters/JaroWinklerFilter.cs
@@ -0,0 +1,37 @@
+﻿using SanteDB.Core.Model.Query;
+using SanteDB.Matcher.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace SanteDB.Matcher.Filters
+{
+    /// <summary>
+    /// Represents a jaro-winkler query extension
+    /// </summary>
+    public class JaroWinklerFilter : IQueryFilterExtension
+    {
+        /// <summary>
+        /// Gets the name of the filter 
+        /// </summary>
+        public string Name => "jarowinkler";
+
+        /// <summary>
+        /// Gets the extension method
+        /// </summary>
+        public MethodInfo ExtensionMethod => typeof(StringDifference).GetRuntimeMethod(nameof(StringDifference.JaroWinkler), new Type[] { typeof(String), typeof(String) });
+
+        /// <summary>
+        /// Compose the function
+        /// </summary>
+        public BinaryExpression Compose(Expression scope, ExpressionType comparison, Expression valueExpression, Expression[] parms)
+        {
+            if (parms.Length != 1) throw new ArgumentOutOfRangeException("jaro-winkler requires one parameter : value=:(jarowinkler|other)comparator");
+            return Expression.MakeBinary(comparison,
+                                Expression.Call(this.ExtensionMethod, scope, parms[0]),
+                                valueExpression);
+        }
+    }
+}
---
+++ SanteDB.Matcher/Transforms/Text/JaroWinklerTransform.cs
@@ -0,0 +1,34 @@
+﻿using SanteDB.Matcher.Util;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SanteDB.Matcher.Transforms.Text
+{
+    /// <summary>
+    /// Jaro-Winkler text transformation 
+    /// </summary>
+    public class JaroWinklerTransform : IBinaryDataTransformer
+    {
+        /// <summary>
+        /// Gets the name of the transform
+        /// </summary>
+        public String Name => "jaro-winkler";
+
+        /// <summary>
+        /// Applies the transform the to specified object
+        /// </summary>
+        public object Apply(object a, object b, params object[] parms)
+        {
+            if (a is String)
+                return ((String)a).JaroWinkler((String)b);
+            else if (a is IEnumerable aEnum && b is IEnumerable bEnum)
+                return aEnum.OfType<String>().SelectMany(sa => bEnum.OfType<String>().Select(sb => sa.JaroWinkler(sb)));
+            else
+                throw new InvalidOperationException("Cannot process this transformation on this type of input");
+
+        }
+    }
+}
